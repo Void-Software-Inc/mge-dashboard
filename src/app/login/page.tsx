202553826b0a70784d61e0338f1677e3875b8d45
devStr0ke <samuel.coelho@toulouse.miage.fr>
2024-06-28 17:09:39 +0200

    Login feature with supabase

---
+++ src/app/login/page.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Image from 'next/image';
+
+import TextInput from '@/components/inputs/textInput';
+
+export default function Login() {
+  const [data, setData] = useState<{
+    email: string,
+    password: string
+  }>({
+    email: '',
+    password: ''
+  })
+
+  const router = useRouter();
+
+  const login = async () => {
+    try {
+      let { data: dataUser, error } = await supabase
+        .auth
+        .signInWithPassword({
+          email: data.email,
+          password: data.password
+        })
+
+      if (dataUser) {
+        router.refresh();
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <main className="flex justify-center md:items-center h-[100vh] bg-white">
+      <div className="p-8 w-full h-[75vh] md:w-[400px] md:shadow-2xl rounded-lg flex flex-col justify-around">
+        <div className="flex flex-col items-start items-center">
+          <h2 className="text-black text-3xl md:text-2xl mb-12 saira font-semibold">Welcome</h2>
+          <Image
+            src="/static/svg/mgelogo.svg"
+            alt="mgelogo"
+            width={150}
+            height={150}
+            priority
+            className="mb-8"
+          />
+        </div>
+        <div></div>
+        <div className='grid gap-4 w-full'>
+          <div className='grid'>
+            <TextInput
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={data.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='grid'>
+            <TextInput
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={data.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex justify-center w-full">
+            <button onClick={login} className="px-4 py-2 bg-orange-500 rounded cursor-pointer w-full saira">Login</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
