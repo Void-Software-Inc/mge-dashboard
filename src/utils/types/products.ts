47f1e9ffb5f37ba8d6a4e156724bf7bc511444bf
devStr0ke <samueltomar@hotmail.com>
2024-07-08 22:42:38 +0200

    Keeping simple datatable

---
+++ src/utils/types/products.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number
+    name: string,
+    type: string,
+    color: string,
+    stock: number,
+    price: number,
+    description: string,
+    image_url: string,
+}
