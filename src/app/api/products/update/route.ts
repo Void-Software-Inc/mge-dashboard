e8e46a283024f93d169cfbea89600d61533b5465
devStr0ke <samueltomar@hotmail.com>
2024-07-15 22:37:40 +0200

    Product update v1

--- src/app/api/products/update/route.ts
+++ src/app/api/products/update/route.ts
@@ -1,20 +1,31 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
+import { formatInTimeZone } from 'date-fns-tz'
 
 export async function PUT(request: NextRequest) {
   const supabase = createClient();
-  const { id, ...updatedData } = await request.json();
+  const { id, last_update, ...updatedData } = await request.json();
+
+  if (!id) {
+    return NextResponse.json({ error: "Product ID is required" }, { status: 400 });
+  }
+
+  const parisDate = formatInTimeZone(new Date(), 'Europe/Paris', "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
 
   const { data, error } = await supabase
     .from("products")
-    .update(updatedData)
-    .eq("id", id)
-    .single();
+    .update({...updatedData, last_update: parisDate})
+    .match({ id })
+    .select();
 
   if (error) {
     console.error("Error updating product:", error);
     return NextResponse.json({ error: "Failed to update product" }, { status: 500 });
   }
 
-  return NextResponse.json({ data });
+  if (data.length === 0) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ data: data[0] });
 }
