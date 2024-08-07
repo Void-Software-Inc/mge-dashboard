78e3a689d0c47d8457e6399102f136272eb18c14
devStr0ke <samueltomar@hotmail.com>
2024-08-07 05:49:44 +0200

    Regroupement of stats in order to optimize fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation';
-import QuoteCreationChart from '@/components/stats/QuoteCreationChart';
-import ProductTypesChart from '@/components/stats/ProductTypesChart';
+import StatsRegroupment from '@/components/stats/StatsRegroupment';
 
 export default async function Home() {
   const supabase = createClient()
@@ -11,15 +10,6 @@ export default async function Home() {
     redirect('/login')
   }
   return (
-    <main className="flex min-h-screen flex-col items-center p-1 md:p-4">
-      <div className='w-full mt-2 grid grid-cols-1 md:grid-cols-2 gap-4'>
-        <div className='w-full px-1 md:px-0'>
-          <ProductTypesChart />
-        </div>
-        <div className='w-full px-1 md:px-0'>
-          <QuoteCreationChart />
-        </div>
-      </div>
-    </main>
+    <StatsRegroupment />
   );
 }
